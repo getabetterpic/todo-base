f81aac6c8579dec3775ea7a616273736c6aa3718
Dan Smith <dan@getabetterpic.com>
2024-10-12 05:09:12 -0400

    Add login component

--- apps/todo-fe/src/app/app.routes.ts
+++ apps/todo-fe/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Route } from '@angular/router';
 import { RegisterComponent } from './users/register.component';
+import { LoginComponent } from './users/login.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
---
+++ apps/todo-fe/src/app/users/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  loginForm = new FormBuilder().group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
