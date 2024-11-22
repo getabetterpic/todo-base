d336e8273d84ab19865d732b06b52931625defc3
Dan Smith <dan@getabetterpic.com>
2024-11-22 03:40:58 -0500

    Frontend specs passing

--- apps/frontend/src/app/users/login.component.spec.ts
+++ apps/frontend/src/app/users/login.component.spec.ts
@@ -1,13 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
+import { UsersService } from './users.service';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let usersService;
 
   beforeEach(async () => {
+    usersService = {
+      login: jest.fn(() => of({})),
+    };
     await TestBed.configureTestingModule({
       imports: [LoginComponent],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: usersService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
--- apps/frontend/src/app/users/register.component.spec.ts
+++ apps/frontend/src/app/users/register.component.spec.ts
@@ -1,13 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterComponent } from './register.component';
+import { UsersService } from './users.service';
+import { of } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let usersService;
 
   beforeEach(async () => {
+    usersService = {
+      register: jest.fn(() => of({})),
+    };
     await TestBed.configureTestingModule({
       imports: [RegisterComponent],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: usersService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
